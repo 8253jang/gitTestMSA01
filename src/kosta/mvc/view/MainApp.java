901e01bcba7d399c2542a8c9724bbd034d7c42c6
kosta <kosta@192.168.0.136>
2020-09-14 16:06:07 +0900

    희정이가 수정함...MainApp

--- src/kosta/mvc/view/MainApp.java
+++ src/kosta/mvc/view/MainApp.java
@@ -3,7 +3,9 @@ package kosta.mvc.view;
 public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("gitTest 시작하기");
-
+        System.out.println("희정이가 넣었다...");
+        System.out.println("2222222");
+        System.out.println("333333333");
 	}
 
 }
