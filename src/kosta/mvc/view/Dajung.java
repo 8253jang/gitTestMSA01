1a55361833d908ce51d5fad8a8082e63f4dd591c
user <user@192.168.0.194>
2020-09-14 15:59:52 +0900

    view 패키지에 Dajung클래스를 추가하고 main 출력내용을 추가 변경했습니다.

--- src/kosta/mvc/view/Dajung.java
+++ src/kosta/mvc/view/Dajung.java
@@ -1,5 +1,8 @@
 package kosta.mvc.view;
 
 public class Dajung {
-	
+	public void sout() {
+		System.out.println("æ»≥Á«œººø‰");
+
+	}
 }
--- src/kosta/mvc/view/MainApp.java
+++ src/kosta/mvc/view/MainApp.java
@@ -3,7 +3,8 @@ package kosta.mvc.view;
 public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("gitTest 시작하기");
-
+		Dajung dd = new Dajung();
+		dd.sout();
 	}
 
 }
